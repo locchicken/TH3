b351b89b86b700211d1684b2b226083ac063238c
Your Name <you@example.com>
2021-10-11 10:36:10 +0700

    first commit

---
+++ ASP.Web/Data/IIdentitySeed.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ASP.Web.Models;
+using ASP.Web.Web.Configuration;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Options;
+
+namespace ASP.Web.Data
+{
+    interface IIdentitySeed
+    {
+        Task Seed(UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager,
+            IOptions<ApplicationSettings> options);
+        Task Seed(UserManager<ApplicationUser> userManager, Func<RoleManager<IdentityRole>> getService);
+        Task Seed(UserManager<ApplicationUser> userManager, Func<Microsoft.AspNetCore.Identity.RoleManager> getService);
+    }
+}
---
+++ ASP.Web/Microsoft/AspNetCore/Identity/RoleManager.cs
@@ -0,0 +1,6 @@
+﻿namespace Microsoft.AspNetCore.Identity
+{
+    internal class RoleManager
+    {
+    }
+}
